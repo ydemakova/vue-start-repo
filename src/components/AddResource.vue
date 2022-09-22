910047d3add6ebd595b3d2c0f9f26947b87755e1
ydemakova <60213335+ydemakova@users.noreply.github.com>
2022-09-22 16:23:34 +0200

    Update AddResource.vue
    Add BaseDialog Component

--- src/components/AddResource.vue
+++ src/components/AddResource.vue
@@ -1,5 +1,14 @@
 <template>
-   <form @submit="addResource">
+   <form @submit.prevent="addResource">
+    <base-dialog v-if="inputIsInvalid" @close="confirmError" title="Invalid Input">
+      <template #default>
+       <p>Unfortunately, at least one input is invalid.</p>
+       <p>Please check all inpunts and make sure you enter valid values</p>
+      </template>
+      <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+      </template>
+</base-dialog>
     <base-card>
      <div class="form-field">
         <label for="">Title: </label>
@@ -19,12 +28,32 @@
 </template>
 
 <script>
+import BaseDialog from './UI/BaseDialog.vue'
     export default{
+  components: { BaseDialog },
      emits: ['add-new-resource'],
-
+     data() {
+    return {
+        inputIsInvalid: false
+    }
+     },
      methods: {
         addResource(){
-            this.$emit('add-new-resource', this.$refs.title.value, this.$refs.description.value, this.$refs.link.value)
+
+            const enteredTitle = this.$refs.title.value
+            const enteredDescription = this.$refs.description.value
+            const enteredLink = this.$refs.link.value
+
+            if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredLink.trim() === '' ) {
+                this.inputIsInvalid = true
+                return
+            }
+
+            this.$emit('add-new-resource', enteredTitle, enteredDescription, enteredLink)
+
+        },
+        confirmError() {
+            this.inputIsInvalid = false
         }
      }
     }
