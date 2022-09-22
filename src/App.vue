a67eb89cb07d387c9307ccb7a610b3f6a726b86d
ydemakova <60213335+ydemakova@users.noreply.github.com>
2022-09-22 15:08:42 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+
+<template>
+    <the-header title="RememberMe"></the-header>
+    <add-resource @add-new-resource="addResource"></add-resource>
+ <ul>
+   <resource-item v-for="resource in storedResources" :key="resource.id" :title="resource.title" :description="resource.description" :link="resource.link"/>
+ </ul>
+</template>
+
+<script>
+    import ResourceItem from './components/ResourceItem.vue'
+    import TheHeader from './components/layout/TheHeader.vue'
+    import AddResource from './components/AddResource.vue'
+    export default{
+    components:{
+        ResourceItem,
+        TheHeader,
+        AddResource
+    },
+      data(){
+        return{
+            storedResources: [ {
+                id: 'official-guide',
+                title: 'Official Guide',
+                description:'The official Vue.js documentation',
+                link: "https://vuejs.org"
+            },
+            {
+                id: 'google',
+                title: 'Google',
+                description:'Learn to google',
+                link: "https://google.org"
+            }
+        ]
+        }
+      },
+      methods: {
+        addResource(title, description, link) {
+            const newResource = {
+                id: title,
+                title,
+                description,
+                link
+            }
+            this.storedResources.unshift(newResource)
+
+        }
+      }
+    }
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+    * {
+      box-sizing: border-box;
+    }
+
+    html {
+      font-family: 'Roboto', sans-serif;
+    }
+
+    body {
+      margin: 0;
+    }
+
+
+    </style>
---
+++ src/components/AddResource.vue
@@ -0,0 +1,60 @@
+<template>
+   <form @submit="addResource">
+    <base-card>
+     <div class="form-field">
+        <label for="">Title: </label>
+        <input type="text" ref="title">
+     </div>
+     <div class="form-field">
+        <label for="">Description: </label>
+        <textarea type="text" ref="description"></textarea>
+     </div>
+     <div class="form-field">
+        <label for="">Link: </label>
+        <input type="text" ref="link">
+     </div>
+     <base-button type="submit">Add resource</base-button>
+    </base-card>
+   </form>
+</template>
+
+<script>
+    export default{
+     emits: ['add-new-resource'],
+
+     methods: {
+        addResource(){
+            this.$emit('add-new-resource', this.$refs.title.value, this.$refs.description.value, this.$refs.link.value)
+        }
+     }
+    }
+</script>
+
+
+
+
+<style scoped>
+    label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: 0.5rem;
+    }
+
+    input,
+    textarea {
+      display: block;
+      width: 100%;
+      font: inherit;
+      padding: 0.15rem;
+      margin-bottom: 2rem;
+      border: 1px solid #ccc;
+    }
+
+    input:focus,
+    textarea:focus {
+      outline: none;
+      border-color: #3a0061;
+      background-color: #f7ebff;
+    }
+
+    </style>
